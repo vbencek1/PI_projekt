ece84fadba214b469734a5aae4bfb5306a3b8a31
Tomislav123 <adzictomislav51@gmail.com>
2019-05-05 15:35:01 +0200

    Dodavanje klase Ribicki Klub i Natjecanje i sve što se u klasama nalazi

---
+++ FishingNet/FishingNet/Natjecanje.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingNet
+{
+    class Natjecanje
+    {
+        public string Naziv { get; set; }
+        public string Pocetak { get; set; }
+        public string Kraj { get; set; }
+        public string Lokacija { get; set; }
+        public bool Aktivnost { get; set; }
+        public string Opis { get; set; }
+        public string KreatorNatjecanja { get; set; }
+        public Natjecanje(string naziv, string pocetak, string kraj, string lokacija, bool aktivnost, string opis, string kreatorNatjecanja)
+        {
+            Naziv = naziv;
+            Pocetak = pocetak;
+            Kraj = kraj;
+            Lokacija = lokacija;
+            Aktivnost = aktivnost;
+            Opis = opis;
+            KreatorNatjecanja = kreatorNatjecanja;
+        }
+        public void IspisNatjecanja()
+        {
+
+        }
+    }
+}
